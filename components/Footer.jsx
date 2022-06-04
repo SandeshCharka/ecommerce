1119cd5688db0235df781086b855552162450e25
sandesh <sd.charka@gmail.com>
2022-06-04 16:59:28 -0700

    name-change

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,10 +4,10 @@ import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
 const Footer = () => {
   return (
     <div className='footer-container'>
-      <p>2022 JSM Headphones All rights reserved</p>
+      <p>2022 Raz3R Headphones All rights reserved</p>
       <p className='icons'>
-        <AiFillInstagram />
-        <AiOutlineTwitter />
+        {/* <AiFillInstagram /> */}
+        {/* <AiOutlineTwitter /> */}
       </p>
     </div>
   );
